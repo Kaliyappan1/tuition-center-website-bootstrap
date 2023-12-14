c92f992fc1344c36dfcb642f844cb55e9a8f7fae
kaliyappan <141757796+Kaliyappan1@users.noreply.github.com>
2023-12-14 20:33:06 +0530

    Add files via upload

--- tution center website bootstrap/js/script.js
+++ tution center website bootstrap/js/script.js
@@ -7,5 +7,13 @@ $(document).ready(function(){
             $('.navbar').removeClass('custom-nav');
         }
     });
+
+$('.navbar-toggler').click(function(){
+    $('.mid-toggle').toggle('fast');
+    $('.navbar-toggler div:first-child').toggleClass('top-toggle');
+    $('.navbar-toggler div:last-child').toggleClass('bottom-toggle');
+    $('.collapse').toggleClass('custom-collapse');
+    
 });
-$('.navbar-toggler').click(functiv)
+
+});
